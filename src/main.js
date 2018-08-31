3faf88b2b67ff2c8b1f8d1e692e01f7ae64b37ee
Eric Rodrich <acron.bn@gmail.com>
2018-08-30 20:54:10 -0500

    Fixed

--- src/main.js
+++ src/main.js
@@ -8,11 +8,7 @@ import store from './store/store';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-<<<<<<< HEAD
 Vue.http.options.root = "http://sdc.test:8080/api/";
-=======
-Vue.http.options.root = "http://localhost:8000/api/";
->>>>>>> 5a936414381366100532e0f7281843f945dfd637
 
 const router = new VueRouter({
   mode: 'history',
